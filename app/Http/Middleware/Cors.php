010306b6a4b9c75e38c0b469749564fdcfdd1715
TuanNH <tuannh7449@co-well.vn>
2023-08-04 15:29:32 +0700

    deploy

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -22,6 +22,7 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', '*');
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');
     }
 }
